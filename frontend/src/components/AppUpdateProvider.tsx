62c576f525f3d7fc72a163b24fae9259872a6f54
rupambairagya44-max <rupambairagya08@gmail.com>
2026-05-25 13:22:36 +0530

    chore: bump version to 1.0.2 to trigger forced updates for 1.0.1 users

--- frontend/src/components/AppUpdateProvider.tsx
+++ frontend/src/components/AppUpdateProvider.tsx
@@ -7,7 +7,7 @@ import CloudLogo from './CloudLogo';
 import api from '@/lib/api';
 
 // Current Hardcoded Version of the Client APK
-const CURRENT_APP_VERSION = '1.0.1';
+const CURRENT_APP_VERSION = '1.0.2';
 
 interface AppUpdateContextType {
   currentVersion: string;
